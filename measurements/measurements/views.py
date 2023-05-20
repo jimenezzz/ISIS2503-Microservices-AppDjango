f0d02297c3306e146ac96733e3d08f37fa088544
jimenezzz <j.jimenez21@uniandes.edu.co>
2023-05-20 12:13:39 -0500

    con Places

--- measurements/measurements/views.py
+++ measurements/measurements/views.py
@@ -16,6 +16,14 @@ def check_variable(data):
             return True
     return False
 
+def check_place(data):
+    r = requests.get(settings.PATH_PLA, headers={"Accept":"application/json"})
+    places = r.json()
+    for place in places:
+        if data["place"] == place["id"]:
+            return True
+    return False
+
 def MeasurementList(request):
     queryset = Measurement.objects.all()
     context = list(queryset.values('id', 'variable', 'value', 'unit', 'place', 'dateTime'))
@@ -25,16 +33,19 @@ def MeasurementCreate(request):
     if request.method == 'POST':
         data = request.body.decode('utf-8')
         data_json = json.loads(data)
-        if check_variable(data_json) == True:
-            measurement = Measurement()
-            measurement.variable = data_json['variable']
-            measurement.value = data_json['value']
-            measurement.unit = data_json['unit']
-            measurement.place = data_json['place']
-            measurement.save()
-            return HttpResponse("successfully created measurement")
+        if check_place(data_json) == True:
+            if check_variable(data_json) == True:
+                measurement = Measurement()
+                measurement.variable = data_json['variable']
+                measurement.value = data_json['value']
+                measurement.unit = data_json['unit']
+                measurement.place = data_json['place']
+                measurement.save()
+                return HttpResponse("successfully created measurement")
+            else:
+                return HttpResponse("unsuccessfully created measurement. Variable does not exist")
         else:
-            return HttpResponse("unsuccessfully created measurement. Variable does not exist")
+            return HttpResponse("unsuccessfully created measurement. Place does not exist")
 
 def MeasurementsCreate(request):
     if request.method == 'POST':
@@ -42,15 +53,18 @@ def MeasurementsCreate(request):
         data_json = json.loads(data)
         measurement_list = []
         for measurement in data_json:
-                    if check_variable(measurement) == True:
-                        db_measurement = Measurement()
-                        db_measurement.variable = measurement['variable']
-                        db_measurement.value = measurement['value']
-                        db_measurement.unit = measurement['unit']
-                        db_measurement.place = measurement['place']
-                        measurement_list.append(db_measurement)
-                    else:
-                        return HttpResponse("unsuccessfully created measurement. Variable does not exist")
+            if check_place(data_json) == True:
+                if check_variable(measurement) == True:
+                    db_measurement = Measurement()
+                    db_measurement.variable = measurement['variable']
+                    db_measurement.value = measurement['value']
+                    db_measurement.unit = measurement['unit']
+                    db_measurement.place = measurement['place']
+                    measurement_list.append(db_measurement)
+                else:
+                    return HttpResponse("unsuccessfully created measurement. Variable does not exist")
+            else:
+                return HttpResponse("unsuccessfully created measurement. Place does not exist")
         
         Measurement.objects.bulk_create(measurement_list)
         return HttpResponse("successfully created measurements")
